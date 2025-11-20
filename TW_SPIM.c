3f3b321c347e6eb25082d5a9140ad5f8022dfcba
TWalsh27 <tjwalsh2006@gmail.com>
2025-11-20 11:56:44 -0500

    Update TW_SPIM.c
    Completed ALU_Operations

--- TW_SPIM.c
+++ TW_SPIM.c
@@ -1,3 +1,10 @@
+/* Functions not yet completed: 
+ - instruction_partition
+ - instruction_decode
+ - rw_memory
+ - write_register
+*/
+
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
     switch(ALUControl) // set up a switch case for 8 distinct ALUControls
@@ -62,6 +69,49 @@ void sign_extend(unsigned offset,unsigned *extended_value)
         *extended_value = offset | 0b11111111111111110000000000000000; // if it's a 1, extend with 16 1s
     else
         *extended_value = offset & 0b00000000000000001111111111111111; // or extend with zeros
+}
+
+int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
+{
+    unsigned operand1 = data1; // op 1 is always data1, as MIPS requires
+    unsigned operand2; // init here
+    int command = 0;
+
+    if (ALUSrc == 0)
+        operand2 = data2;
+    else if (ALUSrc == 1)
+        operand2 = extended_value;
+    else
+        return 1; // not sure if this is truly necessary but halt if somehow a ALUSrc is set outside [0,1]
+
+    if (ALUOp == 7) // R-type
+    {
+        switch (funct)
+        {
+            case 0x20: command = 0; break; // add
+            case 0x22: command = 1; break; // sub
+            case 0x2A: command = 2; break; // SLT signed
+            case 0x2B: command = 3; break; // SLT unsigned
+            case 0x24: command = 4; break; // AND
+            case 0x25: command = 5; break; //OR
+            default: return 1; // invalid R-type instruction
+        }
+    }
+
+    else // I-type
+    {
+        switch (ALUOp)
+        {
+            case 0: command = 0; break; //addi, lw, sw
+            case 1: command = 1; break; // beq, bne
+            case 6: command = 6; break; // lui
+            default: return 1; // invalid I-type instruction
+        }
+    }
+
+    ALU(operand1, operand2, command, ALUresult, Zero); // call ALU with given command
+
+    return 0; // no halt here
 }
 
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
